e55a81a9332e2bfbdf11ce60fb4a3961534cdc74
cw-roy <curtis_roy@msn.com>
2023-05-02 14:31:33 -0700

    Work in progress

--- working_incomplete.py
+++ working_incomplete.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+# Not quite functional, more work needed
+
+"""Using Nicegui front end for FFMpeg file conversion"""
 
 import asyncio
 import os
@@ -7,16 +11,15 @@ import subprocess
 
 from nicegui import app, events, ui
 
-# create data directory
-os.makedirs("data", exist_ok=True)
 
 def convert(source: str):
+    """FFMpeg command "ffmpeg -hide_banner -i <infile> -q:v 0 <outfile>"""
     output_filename = os.path.splitext(source)[0] + "_converted.mp4"
-    subprocess.call(["ffmpeg", "-i", source, "-q:v", "0", output_filename])
+    subprocess.call(["ffmpeg", "-hide_banner", "-i", source, "-q:v", "0", output_filename])
 
 
 async def handle_upload(args: events.UploadEventArguments):
-    os.chdir(os.path.dirname(os.path.abspath(__file__))) # sets path to current directory
+    """Select the file, perform naming operations"""
     if args.type.startswith("video/"):
         video_exts = [".mp4", ".avi", ".mov", ".wmv", ".flv", ".webm", ".mkv"]
         file_ext = os.path.splitext(args.name)[1].lower()
@@ -26,15 +29,19 @@ async def handle_upload(args: events.UploadEventArguments):
             os.chdir("data")
             with open(args.name, "wb") as f:
                 f.write(args.content.read())
-            with ui.spinner("dots", size="xl"):
+            complete = False
+            with ui.spinner(size="xl"):
                 await asyncio.to_thread(convert, args.name)
+                complete = True
+            while not complete:
+                await asyncio.sleep(0.1)
             results.clear()
             with results:
                 ui.video(
                     f"/data/{os.path.splitext(args.name)[0]}_converted.mp4"
                 ).classes("w-96 drop-shadow-md rounded")
-                ui.label("Conversion complete.")
             os.chdir("..")
+            ui.label("Conversion Complete.  Preview conversion above.")
         else:
             ui.notify(
                 "Please upload a video file in a supported format: "
@@ -45,13 +52,13 @@ async def handle_upload(args: events.UploadEventArguments):
     upload.run_method("reset")
 
 
-
+os.makedirs("data", exist_ok=True)
 app.add_static_files("/data", "data")
 
 with ui.column().classes("w-full items-center"):
     ui.label("Convert video to MP4 format").classes("text-3xl m-3")
     upload = ui.upload(
-        label="pick a video file", auto_upload=True, on_upload=handle_upload
+        label="Pick a video file", auto_upload=True, on_upload=handle_upload
     )
     results = ui.row().classes("w-full justify-center mt-6")
 
